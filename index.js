73f4ea8cdec1114e9b5268a194b5a813aa9d45fe
Lenny606 <thomas.kravcik@gmail.com>
2023-02-12 12:04:49 +0100

    js logic for list

--- index.js
+++ index.js
@@ -1,54 +1,28 @@
 'use strict';
+//data
+const arrList = ["Image Generator using OpenAI", "Get Answers using OpenAI"]
 
-// Data needed for a later exercise
-const flights =
-    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+//variables
+const projectsList = document.querySelector(".projects_list")
+const projectsTitle = document.querySelector(".projects_title")
+const screenshotImg = document.querySelector("#screenshot")
 
-// Data needed for first part of the section
-const restaurant = {
-    name: 'Classico Italiano',
-    location: 'Via Angelo Tavanti 23, Firenze, Italy',
-    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-    openingHours: {
-        thu: {
-            open: 12,
-            close: 22,
-        },
-        fri: {
-            open: 11,
-            close: 23,
-        },
-        sat: {
-            open: 0, // Open 24 hours
-            close: 24,
-        },
 
-    },
+//handlers
 
-    order({ num = Math.floor(Math.random() * 100), itemIndex = 1 }) {
-        return `Hello. Youre order #${num} contains ${this.mainMenu[itemIndex]} is ready at ${this.location}`
+function listHandler() {
+    for (const el of arrList) {
+        const li = document.createElement('li')
+        li.addEventListener("click", screenshotHandler)
+        projectsList.appendChild(li).innerHTML = el
+
     }
-
-
-};
-
-const obj = {
-    num: undefined,
-    itemIndex: 8
+    projectsTitle.removeEventListener("click", listHandler)
 }
 
-console.log(restaurant.order(obj))
-// let name = "Bardo"
-// const { names: menus = "not exists" } = restaurant;
-// console.log(menus);
-// ({ name } = restaurant)
+function screenshotHandler() {
+    screenshotImg.className = "screenshot_visible"
+}
 
-// console.log(name);
-
-// const { openingHours } = restaurant
-// console.log(openingHours);
-// const { sat: { open: start, close: end } } = openingHours
-// console.log(start, end);
+projectsTitle.addEventListener("click", listHandler)
